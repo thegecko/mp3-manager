554d1f4271f96b9ea1de41c74021506003e60bd2
thegecko <rob.moran@arm.com>
2024-11-12 15:04:27 -0700

    Add db

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,16 @@
 import { useState } from "preact/hooks";
 
+interface MusicFile {
+    id: number,
+    name: string;
+    folder?: string;
+}
+
 export default function App() {
     const [state, setState] = useState({
-        fileSystem: undefined as FileSystemDirectoryHandle | undefined,
-        files: [] as string[]
+        dbHandle: undefined as FileSystemFileHandle | undefined,
+        dataHandle: undefined as FileSystemDirectoryHandle | undefined,
+        files: [] as MusicFile[]
     });
     //const [fileSystem, setFileSystem] = useState<FileSystemDirectoryHandle>();
 
@@ -11,24 +18,52 @@ export default function App() {
         // Open file picker and destructure the result the first handle
         const fileSystem = await window.showDirectoryPicker({ startIn: "music", mode: 'readwrite' });
         if (fileSystem) {
+            try {
+                const dbHandle = await fileSystem.getFileHandle('db.json');
+                const dataHandle = await fileSystem.getDirectoryHandle('data');
+                const db = await dbHandle.getFile()
+                const contents = await db.text();
+                const files: MusicFile[] = JSON.parse(contents);
+                /*
             const entries = fileSystem.entries();
             const files = [];
             for await (const [name] of entries) {
                 files.push(name);
             }
-            setState({...state, fileSystem, files});
+                */
+                setState({...state, dataHandle, dbHandle, files});
+            } catch (e) {
+                alert('check this is the right folder');
+            }
         }
     }
 
     const saveFile = async (file: File) => {
-        console.log(file.name);
-        if (state.fileSystem) {
-            const fileHandle = await state.fileSystem.getFileHandle(file.name, { create: true });
+        let id = 1;
+        for (const file of state.files) {
+            if (file.id >= id) {
+                id = file.id + 1;
+            }
+        }
+
+        const files = state.files;
+        files.push({ id, name: file.name });
+        
+        if (state.dbHandle) {
+            const writable = await state.dbHandle.createWritable();
+            await writable.write(JSON.stringify(files));
+            await writable.close();
+        }
+
+        if (state.dataHandle) {
+            const fileHandle = await state.dataHandle.getFileHandle(`${id}.data`, { create: true });
             const writable = await fileHandle.createWritable();
             const buffer = await file.arrayBuffer();
             await writable.write(buffer)
             await writable.close();
         }
+
+        setState({...state, files});
     };
 
     const onDrop = async (event: DragEvent) => {
@@ -57,19 +92,24 @@ export default function App() {
         event.preventDefault();
     }
 
-    const fileNodes = state.files.map((file) => <li>{file}</li>);
+    const fileNodes = state.files.map((file) => <li>{`${file.folder || ''}/${file.name} in data/${file.id}.data`}</li>);
 
     return (
-        <div class="text-slate-700 dark:text-slate-500">
-            <button onClick={onClick}>Select drive</button>
-            <ul>{fileNodes}</ul>
+        <>
+            <button
+                class="hover:bg-blue-400 group flex items-center rounded-md bg-blue-500 text-white text-sm font-medium pl-2 pr-3 py-2 shadow-sm"
+                onClick={onClick}>
+                    Select drive
+            </button>
             <div
+                class="hover:border-blue-500 hover:border-solid hover:bg-white hover:text-blue-500 group w-full flex flex-col items-center justify-center rounded-md border-2 border-dashed border-slate-300 text-sm leading-6 text-slate-900 font-medium py-3"
                 id='drop_zone'
                 onDrop={onDrop}
                 onDragOver={onDragOver}
             >
-                <p class="text-lg font-medium">Drag one or more files to this <i>drop zone</i>.</p>
+                <ul>{fileNodes}</ul>
+                Drag one or more files to this drop zone
             </div>
-        </div>
+        </>
     );
 }

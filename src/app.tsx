42f9fd401b758d1f2d79ca1128b264d1c6bd6e6a
thegecko <rob.moran@arm.com>
2024-12-31 01:58:55 +0000

    WiP

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,99 @@
 import { useState } from "preact/hooks";
 import { EsysDatabase, Folder, Track } from "./esys-database";
 
+import update from 'immutability-helper'
+import { useCallback } from 'preact/compat'
+import { Card } from "./card";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+
+const style = {
+	width: 400,
+}
+
+export interface Item {
+	id: number
+	text: string
+}
+
+export interface ContainerState {
+	cards: Item[]
+}
+
 export default function App() {
+    const [cards, setCards] = useState([
+        {
+            id: 1,
+            text: 'Write a cool JS library',
+        },
+        {
+            id: 2,
+            text: 'Make it generic enough',
+        },
+        {
+            id: 3,
+            text: 'Write README',
+        },
+        {
+            id: 4,
+            text: 'Create some examples',
+        },
+        {
+            id: 5,
+            text: 'Spam in Twitter and IRC to promote it (note that this element is taller than the others)',
+        },
+        {
+            id: 6,
+            text: '???',
+        },
+        {
+            id: 7,
+            text: 'PROFIT',
+        },
+    ])
+
+    const moveCard = useCallback((dragIndex: number, hoverIndex: number) => {
+        setCards((prevCards: Item[]) =>
+            update(prevCards, {
+                $splice: [
+                    [dragIndex, 1],
+                    [hoverIndex, 0, prevCards[dragIndex] as Item],
+                ],
+            }),
+        )
+    }, [])
+
+    const newCard = useCallback((hoverIndex: number) => {
+        setCards((prevCards: Item[]) =>
+            update(prevCards, {
+                $splice: [
+                    [prevCards.length + 1, 1],
+                    [hoverIndex, 0,                 {
+                        id: prevCards.length + 1,
+                        text: "new shit",
+                    } as Item],
+                ],
+            }),
+        )
+    }, [])
+
+    const renderCard = useCallback(
+        (card: { id: number; text: string }, index: number) => {
+            return (
+                <Card
+                    key={card.id}
+                    index={index}
+                    id={card.id}
+                    text={card.text}
+                    moveCard={moveCard}
+                    newCard={newCard}
+                    onDrop={handleFileDrop}
+                />
+            )
+        },
+        [],
+    )
+
     const [state, setState] = useState({
         dataHandle: undefined as FileSystemDirectoryHandle | undefined,
         dbHandle: undefined as FileSystemFileHandle | undefined,
@@ -106,8 +198,23 @@ export default function App() {
         </>
     ));
 
+    const handleFileDrop = useCallback((id: number, item: { files: any[] }) => {
+        if (item) {
+            const files = item.files
+            setCards(previous =>
+                previous.map(prev => ({
+                    ...prev,
+                    text: prev.id === id ? files[0].name : prev.text
+                })
+            ));
+        }
+    }, [])
+
     return (
         <>
+            <DndProvider backend={HTML5Backend}>
+                <div style={style}>{cards.map((card, i) => renderCard(card, i))}</div>
+            </DndProvider>
             <button
                 class="hover:bg-blue-400 group flex items-center rounded-md bg-blue-500 text-white text-sm font-medium pl-2 pr-3 py-2 shadow-sm"
                 onClick={onClick}>
